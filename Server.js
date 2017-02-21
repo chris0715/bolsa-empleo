9908a2370e74836a734aef7ef35a4855fdb5aa53
Cristopher <christopher.encarnacion@gmail.com>
2017-02-21 07:22:19 -0500

    se agregaron todos los paquetes

--- Server.js
+++ Server.js
@@ -1,4 +1,16 @@
-import Express from 'express';
+var express= require('express');
 
 
-let app = Express();
+let app = express();
+app.use(express.static(__dirname+'/public'));
+app.set('view engine', 'ejs');
+let serverPort = 5000;
+
+
+
+app.get('/', function(req,res){
+    
+    res.render('index');
+})
+
+app.listen(serverPort, ()=>{console.log('escuchando en el puerto :'+serverPort)})
